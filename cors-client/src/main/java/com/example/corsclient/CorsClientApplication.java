3315f73e12474422bb6c9bc79c0806bf37ce3eeb
songor <chensongyu1028@gmail.com>
2021-07-08 00:20:39 +0800

    cors-client

---
+++ cors-client/src/main/java/com/example/corsclient/CorsClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.corsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CorsClientApplication.class, args);
+  }
+}
